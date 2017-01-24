b004861d1d390d604776a02302ae16f7f90ef173
jackderuntz <17jderuntz@student.nchstx.org>
2017-01-23 22:02:54 -0600

    Added  Gyro Controls

--- src/org/usfirst/frc/team4206/robot/Robot.java
+++ src/org/usfirst/frc/team4206/robot/Robot.java
@@ -3,11 +3,17 @@ package org.usfirst.frc.team4206.robot;
 
 
 import edu.wpi.first.wpilibj.SampleRobot;
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.Spark;
 import com.ctre.CANTalon;
 
@@ -15,7 +21,7 @@ import com.ctre.CANTalon;
  * This is a demo program showing how to use Mecanum control with the RobotDrive class.
  */
 
-public class Robot extends SampleRobot {
+public class Robot extends SampleRobot implements PIDOutput {
 	
     RobotDrive robotDrive;
     Joystick controller;
@@ -26,7 +32,17 @@ public class Robot extends SampleRobot {
     CANTalon rearRight = new CANTalon(6);
     Spark shooter = new Spark(0);
     
-    ADXRS450_Gyro gyro;
+    AHRS ahrs;
+    PIDController turnController;
+    double rotateToAngleRate;
+    
+    static final double kP = 0.1;
+    static final double kI = 0.00;
+    static final double kD = 0.00;
+    static final double kF = 0.00;
+    
+    static final double kToleranceDegrees = 2.0f;
+    
     
     // The channel on the driver station that the joystick is connected to
     final int joystickChannel	= 0;
@@ -38,29 +54,72 @@ public class Robot extends SampleRobot {
         robotDrive.setExpiration(0.1);
 
         controller = new Joystick(joystickChannel);
+        ahrs = new AHRS(SPI.Port.kMXP);
+        turnController = new PIDController(kP, kI, kD, kF, ahrs, this);
+        turnController.setInputRange(-180.0f,  180.0f);
+        turnController.setOutputRange(-1.0, 1.0);
+        turnController.setAbsoluteTolerance(kToleranceDegrees);
+        turnController.setContinuous(true);
         
-        gyro = new ADXRS450_Gyro();
     }
-        
 
     /**
      * Runs the motors with Mecanum drive.
      */
     public void operatorControl() {
         robotDrive.setSafetyEnabled(false);
-        gyro.calibrate();
-        double angle = 0.0;
         while (isOperatorControl() & isEnabled()) {
-        	angle = gyro.getAngle();
+        	boolean rotateToAngle = false;
+            if ( controller.getRawButton(1)) {
+                ahrs.reset();
+            }
+            if ( controller.getRawButton(2)) {
+                turnController.setSetpoint(0.0f);
+                rotateToAngle = true;
+            } else if ( controller.getRawButton(3)) {
+                turnController.setSetpoint(90.0f);
+                rotateToAngle = true;
+            } else if ( controller.getRawButton(4)) {
+                turnController.setSetpoint(179.9f);
+                rotateToAngle = true;
+            } else if ( controller.getRawButton(5)) {
+                turnController.setSetpoint(-90.0f);
+                rotateToAngle = true;
+            }
+            double currentRotationRate;
+            if ( rotateToAngle ) {
+                turnController.enable();
+                currentRotationRate = rotateToAngleRate;
+            } else {
+                turnController.disable();
+                currentRotationRate = controller.getRawAxis(4);
+            }
+            try {
+                /* Use the joystick X axis for lateral movement,          */
+                /* Y axis for forward movement, and the current           */
+                /* calculated rotation rate (or joystick Z axis),         */
+                /* depending upon whether "rotate to angle" is active.    */
+                robotDrive.mecanumDrive_Cartesian(controller.getX(), controller.getY(), 
+                                               currentRotationRate, ahrs.getAngle());
+            } catch( RuntimeException ex ) {
+                DriverStation.reportError("Error communicating with drive system:  " + ex.getMessage(), true);
+            }
+            
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-            robotDrive.mecanumDrive_Cartesian(controller.getX(), controller.getY(), controller.getRawAxis(4), angle);
-            System.out.println(angle);
+            robotDrive.mecanumDrive_Cartesian(controller.getX(), controller.getY(), controller.getRawAxis(4),0);
             
             shooter.set(controller.getRawAxis(3));
             
             Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
         }
     }
+
+
+	@Override
+	public void pidWrite(double output) {
+		rotateToAngleRate = output;
+		
+	}
     
 }
